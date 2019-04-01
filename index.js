f78fd2e5244d5cc959e4ca234a42178dd64a850a
sinkerdPhoutthavong <devn.info@gmail.com>
2019-04-02 00:01:39 +0700

    insert update delete

--- index.js
+++ index.js
@@ -13,4 +13,16 @@ var con = mysql.createConnection({
 con.connect(function(err){
     if(err) throw err;
     console.log("Connected!");
-})
+    // sql = "insert into product (product_name,product_price) values('Keyboard',20000)";//insert to tables
+    // sql = "select * from product"; // get data to show
+    // sql = "delete from product where product_price = 1500";
+    sql = "update product set brand_id= 125554 where product_price=30000";
+    con.query(sql,function(err,result){ // 2 prarameter 
+        if(err) throw err;
+        // console.log("Insert Complete"); 
+        // console.log(result); // log for show data in database;
+        // console.log("Delete Complete"); 
+        console.log("Update Success");
+        
+    });
+});
